e12087b2ba0fe496452d60282394fed60cf1e318
Sergey <sergeyivanyuk89@gmail.com>
2023-01-26 18:07:01 +0400

    refactor

--- server/server.js
+++ server/server.js
@@ -9,8 +9,6 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-console.log(process.env.OPENAI_API_KEY)
-
 const openai = new OpenAIApi(configuration);
 
 const app = express()
@@ -19,7 +17,7 @@ app.use(express.json())
 
 app.get('/', async (req, res) => {
   res.status(200).send({
-    message: 'Hello from CodeX!'
+    message: 'Hello!'
   })
 })
 
